7bc5c9ef41e1d7753745ce4aeb8c89b06f74f2cd
thijsken <220064410+GmsNederland@users.noreply.github.com>
2025-09-08 13:40:45 +0200

    update

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
+// /api/server.js
 const express = require('express');
+const serverless = require('serverless-http');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
 
-// Serveer alle bestanden in 'web', inclusief subfolders
-app.use(express.static(path.join(__dirname, 'web')));
+// Serveer statische bestanden uit 'web' folder
+app.use(express.static(path.join(__dirname, '../web')));
 
-app.listen(PORT, () => {
-  console.log(`Server draait op http://localhost:${PORT}`);
+// Optioneel: root redirect
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../web/index.html'));
 });
-=======
-const express = require('express');
-const path = require('path');
 
-const app = express();
-const PORT = 3000;
-
-// Serveer alle bestanden in 'web', inclusief subfolders
-app.use(express.static(path.join(__dirname, 'web')));
-
-app.listen(PORT, () => {
-  console.log(`Server draait op http://localhost:${PORT}`);
-});
->>>>>>> 0ae52b6ac39f215d9963a96be2b056c70996f8b6
+module.exports.handler = serverless(app);
